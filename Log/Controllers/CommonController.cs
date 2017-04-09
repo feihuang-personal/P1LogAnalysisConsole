428234eea5daba53aea76ab63b3d7ffe5f03c938
Richard.Huang <richard@osooh.com>
2017-04-09 17:57:12 +0800

    Refine ui design

---
+++ Log/Controllers/CommonController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Log.Controllers
+{
+    public class CommonController : Controller
+    {
+        //
+        // GET: /Common/
+
+        public ActionResult LeftMenu()
+        {
+            return PartialView("_LeftMenuTreePartial");
+        }
+
+        public ActionResult TopNavigation()
+        {
+            return PartialView("_TopNavigationPartial");
+        }
+
+        public ActionResult Footer()
+        {
+            return PartialView("_FooterPartial");
+        }
+
+    }
+}
---
+++ Log/Controllers/RepositoryController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Log.Controllers
+{
+    public class RepositoryController : Controller
+    {
+        //
+        // GET: /Repository/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult MyRepositories()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Displays customer list, and select one go to the next page.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Customer()
+        {
+            return View();
+        }
+    }
+}
